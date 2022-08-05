556fc5d19ce3e5224dfa8fc96b747463e477e9a5
LayaneB <50851374+LayaneB@users.noreply.github.com>
2022-08-04 23:40:49 -0300

    Reorganização das requisições e lógica para requisição de cadastro de livro e usuário, deletar livros, pegar informações de um usuário..

--- Frontend/bookreview/src/services/requests.ts
+++ Frontend/bookreview/src/services/requests.ts
@@ -1,13 +1,39 @@
 import axios from "axios"
 import { BooksDTO } from "../interfaces/feed/BooksDTO"
+import { PostBookDTO } from "../interfaces/feed/PostBookDTO"
+import { ProfileDTO } from "../interfaces/profile/ProfileDTO"
+import { SignupDTO } from "../interfaces/signup/SignupDTO"
+import { Token } from "../interfaces/Token"
 
 const baseurl = 'http://localhost:3003/'
 
-export const postRequest = async (endpoint: string, body: { email: string, password: string }, setData: React.Dispatch<React.SetStateAction<string>>, setLoading: React.Dispatch<React.SetStateAction<boolean>>) => {
+const tokenJson = window.sessionStorage.getItem("token")
+const token: Token = tokenJson && JSON.parse(tokenJson)
+
+const headers = {
+    headers: {
+        Authorization: token?.token as string
+    }
+}
+
+// endpoints da entidade user
+
+export const getUser = async (
+    endpoint: string,
+    setData: React.Dispatch<React.SetStateAction<ProfileDTO>>,
+): Promise<void> => {
+    try {
+        const response: any = await axios.get(baseurl + endpoint, headers)
+        setData(response.data.userData)
+    } catch (error: any) {
+        console.log(error.response.data.error)
+    }
+}
+
+export const logIn = async (endpoint: string, body: { email: string, password: string }, setData: React.Dispatch<React.SetStateAction<Token>>, setLoading: React.Dispatch<React.SetStateAction<boolean>>) => {
     setLoading(true)
     try {
         const response: any = await axios.post(baseurl + endpoint, body)
-
         setData(response.data.token)
         setLoading(false)
     } catch (error: any) {
@@ -16,17 +42,24 @@ export const postRequest = async (endpoint: string, body: { email: string, passw
     }
 }
 
-export const getRequest = async (endpoint: string, setData: React.Dispatch<React.SetStateAction<BooksDTO[]>>, setLoading: React.Dispatch<React.SetStateAction<boolean>>) => {
+export const signup = async (endpoint: string, body: SignupDTO, setData: React.Dispatch<React.SetStateAction<Token>>, setLoading: React.Dispatch<React.SetStateAction<boolean>>) => {
+    setLoading(true)
+    console.log(body)
+    try {
+        const response: any = await axios.post(baseurl + endpoint, body)
+        setData(response.data.token)
+        setLoading(false)
+    } catch (error: any) {
+        setLoading(false)
+        console.log(error.response.data.error)
+    }
+}
+
+
+// endpoints da entidade Book
+export const getAllBooks = async (endpoint: string, setData: React.Dispatch<React.SetStateAction<BooksDTO[]>>, setLoading: React.Dispatch<React.SetStateAction<boolean>>) => {
     setLoading(true)
     try {
-
-        const token =window.sessionStorage.getItem("token")
-
-        let headers = {
-            headers: {
-                Authorization: token as string
-            }
-        }
 
         const response: any = await axios.get(baseurl + endpoint, headers)
         setData(response.data.books)
@@ -36,4 +69,47 @@ export const getRequest = async (endpoint: string, setData: React.Dispatch<React
         console.log(error.response.data.error)
     }
 }
+
+
+export const getBookByUser = async (endpoint: string, setData: React.Dispatch<React.SetStateAction<BooksDTO[]>>, setLoading: React.Dispatch<React.SetStateAction<boolean>>) => {
+    setLoading(true)
+    try {
+        const response: any = await axios.get(baseurl + endpoint, headers)
+        setData(response.data.books)
+        setLoading(false)
+    } catch (error: any) {
+        setLoading(false)
+        console.log(error.response.data.error)
+    }
+}
+
+export const postBook = async (endpoint: string, body: PostBookDTO, setReload: React.Dispatch<React.SetStateAction<boolean>>, reload: boolean, setLoading: React.Dispatch<React.SetStateAction<boolean>>) => {
+    setLoading(true)
+    try {
+
+        console.log(body)
+
+        await axios.post(baseurl + endpoint, body, headers)
+        setReload(!reload)
+        setLoading(false)
+    } catch (error: any) {
+        setLoading(false)
+        console.log(error.response.data.error)
+    }
+}
+
+export const deleteBookById = async (endpoint: string, setLoading: React.Dispatch<React.SetStateAction<boolean>>, setReload: React.Dispatch<React.SetStateAction<boolean>>, reload: boolean) => {
+    setLoading(true)
+    try {
+    
+        await axios.delete(baseurl + endpoint, headers)
+        setReload(!reload)
+        setLoading(false)
+    } catch (error: any) {
+        setLoading(false)
+        console.log(error.response.data.error)
+    }
+}
+
+
 
